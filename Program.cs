caa5a2f27fd6e9bb7622b1c2098f53d4296ccdd0
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-09-01 07:36:24 -0500

    Added switch statement to Program.Main

--- Program.cs
+++ Program.cs
@@ -5,24 +5,49 @@
         static Random random = new Random();
         static void Main(string[] args)
         {
-            SwordDamage swordDamage = new SwordDamage(RollDice());
+            SwordDamage swordDamage = new SwordDamage(RollDice(3));
+            ArrowDamage arrowDamage = new ArrowDamage(RollDice(1));
             while (true)
             {
                 Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, " +
                                 "3 for both, anything else to quit: ");
                 char key = Console.ReadKey().KeyChar;
                 if (key != '0' && key != '1' && key != '2' && key != '3') return;
-                swordDamage.Roll = RollDice();
-                swordDamage.Magic = (key == '1' || key == '3');
-                swordDamage.Flaming = (key == '2' || key == '3');
-                Console.WriteLine($"\nRolled {swordDamage.Roll} for {swordDamage.Damage} HP\n");
+                Console.Write("\nS for Sword, A for arrow, anything else to quit: \n");
+                char weaponKey = Char.ToUpper(Console.ReadKey().KeyChar);
+                switch (weaponKey)
+                {
+                    case 'S':
+
+                        swordDamage.Roll = RollDice(3);
+                        swordDamage.Magic = (key == '1' || key == '3');
+                        swordDamage.Flaming = (key == '2' || key == '3');
+                        Console.WriteLine($"\nRolled {swordDamage.Roll} for {swordDamage.Damage} HP\n");
+
+                        break;
+                    case 'A':
+                        arrowDamage.Roll = RollDice(1);
+                        arrowDamage.Magic = (key == '1' || key == '3');
+                        arrowDamage.Flaming = (key == '2' || key == '3');
+                        Console.WriteLine($"\nRolled {arrowDamage.Roll} for {arrowDamage.Damage} HP\n");
+
+                        break;
+                    default:
+                        return;
+                }
+
             }
         }
 
 
-        private static int RollDice()
+        private static int RollDice(int numberOfRolls)
         {
-            return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            int rollTotal = 0;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                rollTotal += random.Next(1, 7);
+            }
+            return rollTotal;
         }
     }
 }
